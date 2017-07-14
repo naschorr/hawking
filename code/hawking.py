2f35d51b011cebabb51a17a3806804e981c1c454
Nick Schorr <naschorr@gmail.com>
2017-07-14 19:54:44 -0400

    Added some new preset speeches

--- code/hawking.py
+++ code/hawking.py
@@ -16,6 +16,7 @@ if not discord.opus.is_loaded():
     discord.opus.load_opus('opus')
 
 ## Config
+## Todo: fix this hot mess
 ACTIVATION_STRING = "\\"
 DESCRIPTION = "A DECTalk interface for Discord (Alpha)"
 ROOT_DIR = os.sep.join(os.path.realpath(__file__).split(os.path.sep)[:-2])
@@ -26,8 +27,9 @@ DECTALK_DIR_PATH = os.path.dirname(os.path.abspath(__file__))
 DECTALK_EXE = "say.exe"
 DECTALK_EXE_PATH = os.sep.join([DECTALK_DIR_PATH, DECTALK_EXE])
 DECTALK_ARGS = "-w"
-DECTALK_PREPEND = "[:phone on] "
+DECTALK_PREPEND = "[:phoneme on] "
 DECTALK_APPEND = ""
+DECTALK_PAUSE_SIM = "    "
 DECTALK_FILE_OUTPUT_FORMAT = "{}.wav"
 TEMP_DIR = "temp"
 TEMP_DIR_PATH = os.sep.join([ROOT_DIR, TEMP_DIR])
@@ -92,6 +94,7 @@ class Speech:
         self.prepend_string = DECTALK_PREPEND
         self.append_string = DECTALK_APPEND
         self.temp_dir_path = TEMP_DIR_PATH
+        self.dectalk_pause_sim = DECTALK_PAUSE_SIM
         self.dectalk_file_output = DECTALK_FILE_OUTPUT_FORMAT
         self.char_limit = CHAR_LIMIT
 
@@ -157,7 +160,7 @@ class Speech:
             self.dectalk_args,  ## Todo: fix this naming
             temp_file_path,
             self.prepend_string,
-            message.replace('"', ""),
+            message.replace("\n", self.dectalk_pause_sim).replace('"', ""), # Todo: dedicated message parser
             self.append_string
         )
 
@@ -234,7 +237,6 @@ class Speech:
         if(state.voice_client is None):
             await self.create_voice_client(voice_channel)
 
-        message = message.replace('\n', ' ')    # Newlines trip up the tts
         try:
             ## Create a .wav file of the message
             wav_path = await self.save_wav(message, ctx.message.server, ctx.message.author)
@@ -269,8 +271,8 @@ class SpeechPreset:
     async def skeletons(self, ctx):
         """Spooky and scary"""
         message = (
-            "[spuh<300,19>kiy<300,19>skeh<300,18>riy<300,18>skeh<300,11>lleh<175,14>tih<200,11>ns]"
-            "[seh<300,11>nd][shih<100,19>ver<500,19>sdaw<300,18>nyur<300,18>spay<300,11>n]"
+            "[spuh<300,19>kiy<300,19>skeh<300,18>riy<300,18>skeh<300,11>lleh<175,14>tih<200,11>ns]. "
+            "[seh<300,11>nd][shih<100,19>ver<500,19>sdaw<300,18>nyur<300,18>spay<300,11>n]. "
             "[shriy<300,19>kiy<300,19>ng][skow<300,18>swih<300,18>ll]"
             "[shah<300,11>kyur<300,14>sow<300,11>ll]"
             "[siy<300,14>llyur<300,16>duh<300,13>mtuh<300,14>nay<300,11>t]"
@@ -358,6 +360,40 @@ class SpeechPreset:
             "lxao<600,23>lxao<200,21>lxao<1600,23>][lxao<400,16>][lxao<400,16>][lxao<800,18>]"
             "[lxao<400,18>][lxao<400,20>][lxao<800,21>][lxao<400,21>][lxao<400,23>][lxao<800,25>]"
             "[lxao<400,27>][lxao<400,28>][lxao<800,30>]"
+        )
+        await self.speech.say.callback(self.speech, ctx, message=message)
+
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def allstar(self, ctx):
+        """It's all ogre now"""
+        message = (
+            "[suh<600,19>bah<300,26>diy<200,23>wow<300,23>]uce[tow<300,21>miy<250,19>]"
+            "[thuh<250,19>wer<450,24>]urd[ih<100,23>]s[gao<250,23>nah<200,21>]roll[miy<200,19>]"
+            "[ay<200,19>][ey<200,26>]int[thuh<200,23>]sharp[eh<200,21>]estool[ih<200,19>]nthuh[sheh<400,16>][eh<300,14>]ed. "
+            "[shiy<300,19>][wah<300,19>][lxuh<200,26>][kih<200,23>][kay<300,23>][nah<300,21>][duh<250,21>]uhm"
+            "[wih<250,19>][fer<250,19>][fih<450,24>]ing[gur<200,23>][ah<250,23>][ner<200,21>][thuh<200,21>]uhm"
+            "[ih<200,19>][thuh<200,19>][shey<400,26>][puh<200,23>]fan[_<50,21>]L[ah<200,19>]ner[for<400,21>][eh<300,14>]ed"
+        )
+        await self.speech.say.callback(self.speech, ctx, message=message)
+
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def careless(self, ctx):
+        """I have pop pop in the attic"""
+        message = (
+            "[dah<400,29>dah<200,27>dah<400,22>dah<300,18>dah<500,29>dah<200,27>dah<400,22>dah<400,18>]. "
+            "[dah<400,25>dah<200,23>dah<400,18>dah<300,15>dah<500,25>dah<200,23>dah<400,18>]"
+        )
+        await self.speech.say.callback(self.speech, ctx, message=message)
+
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def cena(self, ctx):
+        """And his name is!"""
+        message = (
+            "[bah<300,20>dah<200,22>dah<200,18>dah<600,20>]. "
+            "[bah<400,23>dah<200,22>dah<200,18>dah<700,20>]"
         )
         await self.speech.say.callback(self.speech, ctx, message=message)
 
