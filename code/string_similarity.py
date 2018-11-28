b8a171d8a6c7a7558282f6fb784de07ac8c0d286
Nick Schorr <naschorr@gmail.com>
2018-11-27 19:53:26 -0500

    Implemented invalid command suggestions. Fixes #3
    - Updated config.json to include new items for tweaking string similarity algorithm and threshold
    - Added StringSimilarity class to handle similarity calculations
    - Updated on_command_error to suggest a new command when an invalid command was provided
    - Version bump

---
+++ code/string_similarity.py
@@ -0,0 +1,38 @@
+from difflib import SequenceMatcher
+
+import utilities
+
+## Config
+CONFIG_OPTIONS = utilities.load_config()
+
+
+class StringSimilarity:
+    ## https://stackoverflow.com/questions/17388213/find-the-similarity-metric-between-two-strings
+    ## https://stackoverflow.com/questions/6690739/fuzzy-string-comparison-in-python-confused-with-which-library-to-use
+
+    @staticmethod
+    def _calcJaroWinkleDistance(stringA, stringB):
+        raise NotImplementedError(
+            "Jaro-Winkle distance calculation hasn't been implemented. Use difflib implementation"
+        )
+
+    @staticmethod
+    def _calcDamerauLevenshteinDistance(stringA, stringB):
+        raise NotImplementedError(
+            "Damerau-Levenshtein distance calculation hasn't been implemented. Use difflib implementation"
+        )
+
+    @staticmethod
+    def _calcDifflibDistance(stringA, stringB):
+        return SequenceMatcher(None, stringA, stringB).ratio()
+
+    @staticmethod
+    def similarity(stringA, stringB):
+        similarity_algorithm = CONFIG_OPTIONS.get("string_similarity_algorithm")
+
+        if (similarity_algorithm == "jaro-winkler"):
+            return StringSimilarity._calcJaroWinkleDistance(stringA, stringB)
+        elif (similarity_algorithm == "damerau–levenshtein"):
+            return StringSimilarity._calcDamerauLevenshteinDistance(stringA, stringB)
+        else:
+            return StringSimilarity._calcDifflibDistance(stringA, stringB)
