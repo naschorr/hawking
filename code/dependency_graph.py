2d3653b976a5982fe5a639e89865fb4db62bdfe7
Nick Schorr <naschorr@gmail.com>
2021-03-11 20:28:14 -0500

    Initial implementation of the dependency graph logic

---
+++ code/dependency_graph.py
@@ -0,0 +1,66 @@
+import logging
+
+import utilities
+
+## Config
+CONFIG_OPTIONS = utilities.load_config()
+
+## Logging
+logger = utilities.initialize_logging(logging.getLogger(__name__))
+
+
+class DependencyNode:
+    def __init__(self, cls):
+        self.cls = cls
+        self.name: str = cls.__name__
+        self.children: list = []
+        self.parents: list = []
+        self.loaded: bool = False
+
+
+class DependencyGraph:
+    def __init__(self):
+        self.roots: list = []   # list of (1 or more) root DependencyNodes that form a dependency chain
+        self._node_map: dict = {}    # dictionary of class names to nodes that've been inserted into the graph
+        self._orphaned_node_map: dict = {}   # dictionary of class names (that haven't been inserted into the graph) to list of nodes that require that non-existant class
+
+    ## Methods
+
+    def insert(self, cls, dependencies = []):
+        class_name = cls.__name__
+
+        ## Don't insert duplicates
+        if (class_name in self._node_map):
+            logger.warn('Unable to insert {}, as it\'s already been added.'.format(class_name))
+            return
+
+        ## Build initial node & update mappings
+        node = DependencyNode(cls)
+        self._node_map[class_name] = node
+
+        ## Handle any orphaned children that depend on this class
+        if (class_name in self._orphaned_node_map):
+            orphaned_children = self._orphaned_node_map[class_name]
+
+            for child in orphaned_children:
+                node.children.append(child)
+                child.parents.append(node)
+
+            del self._orphaned_node_map[class_name]
+
+        ## Process the dependencies by searching for existing nodes, otherwise populate the orphaned child map
+        for dependency in dependencies:
+            if (dependency in self._node_map):
+                dependency_node = self._node_map[dependency]
+
+                node.parents.append(dependency_node)
+                dependency_node.children.append(node)
+            else:
+                if (dependency in self._orphaned_node_map):
+                    self._orphaned_node_map[dependency].append(node)
+                else:
+                    self._orphaned_node_map[dependency] = [node]
+
+        ## Add it to the list of root nodes
+        if (len(node.parents) and len(dependencies) == 0):
+            self.roots.append(node)
