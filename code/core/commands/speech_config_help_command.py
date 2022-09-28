a749b2acfe64db07f5b7ce8fb66a1690d2348163
Nick Schorr <naschorr@gmail.com>
2022-09-28 03:53:55 -0700

    Upgraded the `SpeechConfigHelpCommand` to use an embed that's more consistent with the recent `SocialInviteCommand` changes

--- code/core/commands/speech_config_help_command.py
+++ code/core/commands/speech_config_help_command.py
@@ -14,13 +14,14 @@ LOGGER = Logging.initialize_logging(logging.getLogger(__name__))
 
 class SpeechConfigHelpCommand(Cog):
 
-    HAWKING_SPEECH_CONFIG_URL = 'https://github.com/naschorr/hawking/blob/master/docs/configuring_speech.md'
+    HAWKING_SPEECH_CONFIG_URL = "https://github.com/naschorr/hawking/blob/master/docs/configuring_speech.md"
 
     def __init__(self, bot, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.bot = bot
 
+        self.repo_url = CONFIG_OPTIONS.get("bot_repo_url")
         self.dynamo_db = dynamo_manager.DynamoManager()
 
     ## Methods
@@ -31,10 +32,31 @@ class SpeechConfigHelpCommand(Cog):
 
         # self.dynamo_db.put_message_context(ctx)
 
-        text = (
+        description = (
             f"Take a look at Hawking's [speech configuration documentation]({self.HAWKING_SPEECH_CONFIG_URL}). It's "
             "got everything you need to get started with tweaking Hawking to do pretty much anything you'd like!"
         )
-        embed = discord.Embed(description=text)
 
-        await interaction.response.send_message(f"Hey <@{interaction.user.id}>,", embed=embed, ephemeral=True)
+        embed = discord.Embed(
+            title="Hawking Speech Configuration",
+            description=description,
+            url=self.HAWKING_SPEECH_CONFIG_URL
+        )
+        embed.set_thumbnail(url=self.bot.user.avatar.url)
+
+        view = discord.ui.View()
+
+        view.add_item(discord.ui.Button(
+            style=discord.ButtonStyle.link,
+            label="Read the Speech Config docs",
+            url=self.HAWKING_SPEECH_CONFIG_URL
+        ))
+
+        if (self.repo_url is not None):
+            view.add_item(discord.ui.Button(
+                style=discord.ButtonStyle.link,
+                label="Visit Hawking on GitHub",
+                url=self.repo_url
+            ))
+
+        await interaction.response.send_message(embed=embed, view=view, ephemeral=True)
