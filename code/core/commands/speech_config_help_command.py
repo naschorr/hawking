db8596d021111d31b2194267db01c3857fd6de3d
Nick Schorr <naschorr@gmail.com>
2022-09-28 01:06:51 -0700

    Migrated `SpeechConfigHelpCommand` to use slash commands

--- code/core/commands/speech_config_help_command.py
+++ code/core/commands/speech_config_help_command.py
@@ -1,4 +1,3 @@
-import inspect
 import logging
 
 from common.configuration import Configuration
@@ -7,7 +6,6 @@ from common.logging import Logging
 from common.module.module import Cog
 
 import discord
-from discord.ext import commands
 
 ## Config & logging
 CONFIG_OPTIONS = Configuration.load_config()
@@ -22,19 +20,21 @@ class SpeechConfigHelpCommand(Cog):
         super().__init__(*args, **kwargs)
 
         self.bot = bot
+
         self.dynamo_db = dynamo_manager.DynamoManager()
 
     ## Methods
 
-    @commands.command(no_pm=True)
-    async def speech_config(self, ctx):
-        '''Posts a link to the speech documentation.'''
-        
-        self.dynamo_db.put_message_context(ctx)
+    @discord.app_commands.command(name="speech_config")
+    async def speech_config_command(self, interaction: discord.Interaction):
+        """Posts a link to the speech configuration documentation."""
 
-        embed = discord.Embed(
-            description='Take a look at Hawking\'s [speech configuration documentation]({}), it\'s got everything you need to get started with tweaking Hawking to do pretty much anything you\'d like.'
-                .format(self.HAWKING_SPEECH_CONFIG_URL)
+        # self.dynamo_db.put_message_context(ctx)
+
+        text = (
+            f"Take a look at Hawking's [speech configuration documentation]({self.HAWKING_SPEECH_CONFIG_URL}). It's "
+            "got everything you need to get started with tweaking Hawking to do pretty much anything you'd like!"
         )
+        embed = discord.Embed(description=text)
 
-        await ctx.send('Hey <@{}>,'.format(ctx.message.author.id), embed=embed)
+        await interaction.response.send_message(f"Hey <@{interaction.user.id}>,", embed=embed, ephemeral=True)
